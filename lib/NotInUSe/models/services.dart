4f669d90411e4a838e3b314c16381fcea700ca5b
saffi <f2019-281@bnu.edu.pk>
2023-03-09 02:46:59 +0500

    ...

---
+++ lib/NotInUSe/models/services.dart
@@ -0,0 +1,11 @@
+class Services{
+  String? uid;
+  String? servicetitle;
+  String? servicetype;
+  String? servicedescription;
+  String? price;
+  String? vetids;
+  String? ClinicName;
+
+  Services({this.uid,this.servicetitle,this.servicetype,this.servicedescription,this.price,this.vetids,this.ClinicName});
+}
